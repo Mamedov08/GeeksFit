a352c0790d0877e6a760509c750b67eea768663e
London069 <alcoreAkaEzetight1337@gmail.com>
2023-03-17 11:17:58 +0600

    successpasswordfragment bug's

--- app/src/main/java/com/example/geeksfit/ui/dashboard/SuccesPasswordFragment.kt
+++ app/src/main/java/com/example/geeksfit/ui/dashboard/SuccesPasswordFragment.kt
@@ -1,20 +1,43 @@
-package com.example.geeksfit.ui.dashboard
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.geeksfit.R
+import com.example.geeksfit.databinding.FragmentSuccesPasswordBinding
+import com.example.geeksfit.ui.gim.ui.GimFragment
+import com.google.android.material.button.MaterialButton
 
-class SuccesPasswordFragment : Fragment() {
+class SuccessPasswordFragment : Fragment() {
+
+    private var _binding: FragmentSuccesPasswordBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_succes_password, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSuccesPasswordBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val changePasswordTextView: TextView = binding.scTextView
+        val successTextView: TextView = binding.scTextView2
+        val enterPasswordButton: MaterialButton = binding.scButton
+
+        enterPasswordButton.setOnClickListener {
+            val gimFragment : GimFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.GimFragment, gimFragment)
+                .commit()
+        }
+
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
